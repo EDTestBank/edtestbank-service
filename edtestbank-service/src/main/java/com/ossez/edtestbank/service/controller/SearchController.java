cc0d462ce6f4a90b8ea0de77299542d0a289fdbf
YuCheng Hu <yuchenghu@ossez.com>
2020-12-13 15:22:16 -0500

    OSSEZ-80 Update logback to send log to datadog

--- edtestbank-service/src/main/java/com/ossez/edtestbank/service/controller/SearchController.java
+++ edtestbank-service/src/main/java/com/ossez/edtestbank/service/controller/SearchController.java
@@ -1,18 +1,13 @@
 package com.ossez.edtestbank.service.controller;
 
 
-import com.microsoft.applicationinsights.TelemetryClient;
-
-import com.ossez.edtestbank.common.dao.factories.QuestionFactory;
 import com.ossez.edtestbank.common.models.orm.QuestionIndex;
-import com.ossez.edtestbank.common.models.response.SearchResponse;
 import com.ossez.edtestbank.common.service.QuestionService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.UUID;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Search Endpoint
