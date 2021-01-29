0e22c0fe156a2d4e53746f7e9e29925e9e037b6f
YuCheng Hu <yuchenghu@ossez.com>
2021-01-29 11:12:32 -0500

    OSSEZ-86 初始化提交

--- edtestbank-common/src/main/java/com/ossez/edtestbank/common/dao/repositories/REListingRepository.java
+++ edtestbank-common/src/main/java/com/ossez/edtestbank/common/dao/repositories/REListingRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 /**
- *
+ * REListingRepository
  */
 @Repository
 public interface REListingRepository extends JpaRepository<REListing, Long> {
